13120200825ad7af7eedff4def02ee59f0aaf358
Your Name <josephmoscoso70@hotmail.com>
2018-05-11 14:22:48 -0600

    Se creo clase berserk y clase arquero

--- Lab2_JosephMoscoso/src/lab2_josephmoscoso/arquero.java
+++ Lab2_JosephMoscoso/src/lab2_josephmoscoso/arquero.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2_josephmoscoso;
 
 /**
@@ -10,5 +5,81 @@ package lab2_josephmoscoso;
  * @author Joseph
  */
 public class arquero {
+    private int hp=150;
+    private int ad=50;
+    private int eficacia=85;
+    private int velocidad=15;
+    private int critic=60;
+    private int suerte=20;
+    private String grito;
+
+    public arquero() {
+    }
+    
+    public arquero(int hp, int ad, int eficacia, int velocidad, int critic, int suerte, String grito) {
+        this.hp = hp;
+        this.ad = ad;
+        this.eficacia = eficacia;
+        this.velocidad = velocidad;
+        this.critic = critic;
+        this.suerte = suerte;
+        this.grito = grito;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAd() {
+        return ad;
+    }
+
+    public void setAd(int ad) {
+        this.ad = ad;
+    }
+
+    public int getEficacia() {
+        return eficacia;
+    }
+
+    public void setEficacia(int eficacia) {
+        this.eficacia = eficacia;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getCritic() {
+        return critic;
+    }
+
+    public void setCritic(int critic) {
+        this.critic = critic;
+    }
+
+    public int getSuerte() {
+        return suerte;
+    }
+
+    public void setSuerte(int suerte) {
+        this.suerte = suerte;
+    }
+
+    public String getGrito() {
+        return grito;
+    }
+
+    public void setGrito(String grito) {
+        this.grito = grito;
+    }
     
 }
